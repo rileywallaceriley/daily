13d54233f25027df4deab1a45cb8ddde6f885672
rileywallaceriley <67809682+rileywallaceriley@users.noreply.github.com>
2024-03-27 13:34:15 -0400

    Update create.py

--- create.py
+++ create.py
@@ -10,6 +10,7 @@ def setup_page_layout():
     """Displays a randomly selected header image and a welcoming message."""
     st.image(get_random_image())
     st.write("Welcome to Vibe Cat; share your vibe, and let's find some tunes together. Need to switch tabs? No worry, I'll keep your playlist safe here so you can visit it throughout the day. Any errors? Just reload. Meow.")
+    st.write("Meet my Cat Dad: instagram.com/rileywallace")
 
 def get_random_image():
     """Returns a random image URL from a predefined list."""
